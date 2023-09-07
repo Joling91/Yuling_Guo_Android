cb7f7478a653ac48dd9b3fded887f8f0e34c0c8e
Yuling Guo <guo00125@algonquinlive.com>
2023-09-07 16:15:57 -0400

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Yuling Guo's Android Labs"
+include(":app")
