147c1ce82d3b92ab717b92fc154fca3f01d365b5
Yuling Guo <guo00125@algonquinlive.com>
2023-10-31 09:50:24 -0400

    create chat room activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,9 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     //implementation(fileTree(mapOf("dir" to "C:\\Users\\jolin\\AppData\\Local\\Android\\Sdk\\platforms\\android-33", "include" to listOf("*.aar", "*.jar"))))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
