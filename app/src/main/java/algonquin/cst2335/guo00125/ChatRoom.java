400a026ce56e4e46665b8fb40ae320ab7ec2d52c
Yuling Guo <guo00125@algonquinlive.com>
2023-11-01 11:19:52 -0400

    modified

--- app/src/main/java/algonquin/cst2335/guo00125/ChatRoom.java
+++ app/src/main/java/algonquin/cst2335/guo00125/ChatRoom.java
@@ -42,6 +42,7 @@ public class ChatRoom extends AppCompatActivity {
 
             @Override
             public int getItemCount() {
+
                 return messages.size();
             }
         });
